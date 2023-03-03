415d2d8f4541114ab379fbf7710ae84403b6d6bc
Enismasic <emasic@hawk.iit.edu>
2023-03-02 20:35:04 -0600

    fixing js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const form = document.getElementById('raffle-form');
+const form = document.getElementById('raffle');
 form.addEventListener('submit', (event) => {
   event.preventDefault();
   alert('You have been raffled');
