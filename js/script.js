d1e74a56819bfc7717564f5d2a1c82afa2a01597
Enismasic <emasic@hawk.iit.edu>
2023-03-02 20:54:08 -0600

    alert change

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const form = document.getElementById('raffle');
 form.addEventListener('submit', (event) => {
   event.preventDefault();
-  alert('You have been entered in the raffle');
+  alert('You have been raffled');
 });
