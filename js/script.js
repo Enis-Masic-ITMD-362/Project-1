d44e80faecb52ab73ccef9e5e536c0511c16d39b
Enismasic <emasic@hawk.iit.edu>
2023-03-02 20:45:16 -0600

    fixed error

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const form = document.getElementById('raffle');
 form.addEventListener('submit', (event) => {
   event.preventDefault();
-  alert('You have been raffled');
+  alert('You have been entered in the raffle');
 });
